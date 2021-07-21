eb9ee374501656ed7f1a74cac955e6d5f2b9a6ca
Christopher Yarp <cyarp@eecs.berkeley.edu>
2021-07-21 23:31:43 +0000

    alternate version of my memcpy

--- src/avxMemcpy.h
+++ src/avxMemcpy.h
@@ -13,26 +13,33 @@
     int fp64Copies = bytes/8;\
     bytes -= fp64Copies*8;\
     \
-    char* dst = (char*) DST;\
-    char* src = (char*) SRC;\
-    \
     double* dstDouble = (double*) dst;\
     double* srcDouble = (double*) src;\
     for(int i = 0; i<avx256Copies; i++){\
-        __m256d ldVal =  _mm256_loadu_pd(srcDouble+i*4);\
-        _mm256_storeu_pd (dstDouble+i*4, ldVal);\
+        __m256d ldVal =  _mm256_loadu_pd(srcDouble);\
+        _mm256_storeu_pd (dstDouble, ldVal);\
+        srcDouble += 4;\
+        dstDouble += 4;\
     }\
     for(int i = 0; i<avx128Copies; i++){\
-        __m128d ldVal =  _mm_loadu_pd(srcDouble+avx256Copies*4+i*2);\
-        _mm_storeu_pd (dstDouble+avx256Copies*4+i*2, ldVal);\
+        __m128d ldVal =  _mm_loadu_pd(srcDouble);\
+        _mm_storeu_pd (dstDouble, ldVal);\
+        srcDouble += 2;\
+        dstDouble += 2;\
     }\
     for(int i = 0; i<fp64Copies; i++){\
-        double ldVal = srcDouble[avx256Copies*4+avx128Copies*2+i];\
-        dstDouble[avx256Copies*4+avx128Copies*2+i] = ldVal;\
+        double ldVal = *srcDouble;\
+        *dstDouble = ldVal;\
+        srcDouble++;\
+        dstDouble++;\
     }\
+    char* dst = (char*) dstDouble;\
+    char* src = (char*) srcDouble;\
     for(int i = 0; i<bytes; i++){\
-        char ldVal = src[(avx256Copies*4+avx128Copies*2+fp64Copies)*8+i];\
-        dst[(avx256Copies*4+avx128Copies*2+fp64Copies)*8+i] = ldVal;\
+        char ldVal = *src;\
+        *dst = ldVal;\
+        src++;\
+        dst++;\
     }\
 }\
 }
