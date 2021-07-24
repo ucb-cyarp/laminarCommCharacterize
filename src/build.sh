d710d30a6ab132e72092f324ea59280cb781e216
Christopher Yarp <cyarp@eecs.berkeley.edu>
2021-07-24 00:59:31 +0000

    Forgot how to write bash there!

--- src/build.sh
+++ src/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-if [[ -z  ${FIFO_BLK_SIZE_CPLX_FLOAT} ]]
+if [[ -z  ${FIFO_BLK_SIZE_CPLX_FLOAT} ]]; then
     FIFO_BLK_SIZE_CPLX_FLOAT=128
 fi
 
-if [[ -z ${TRANSACTIONS_BLKS} ]]
+if [[ -z ${TRANSACTIONS_BLKS} ]]; then
     TRANSACTIONS_BLKS=20000000
 fi
 
-if [[ -z ${CC} ]]
+if [[ -z ${CC} ]]; then
     CC=clang
 fi
 
